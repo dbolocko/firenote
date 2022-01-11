d681ed12ce67550a600b381b4304584b8a78690b
dbolocko <35933445+dbolocko@users.noreply.github.com>
2022-01-11 22:19:28 +0300

    1

--- app/src/main/java/com/ccc/firenote/MainActivity.java
+++ app/src/main/java/com/ccc/firenote/MainActivity.java
@@ -9,22 +9,25 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 
 public class MainActivity extends Activity {
-
+    ImageView imageView;
+    ImageView imageView1;
+    EditText t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+        imageView = (ImageView) findViewById(R.id.imageView2);
+        imageView1 = (ImageView) findViewById(R.id.imageView);
+        t1 = (EditText) findViewById(R.id.editTextTextMultiLine);
+        t1.setBackgroundResource(R.drawable.border);
         Glide.with(this).asGif().load(R.drawable.fire65).into(imageView);
     }
 
     public void fire_note(View vvv)
     {
-        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
         imageView.setVisibility(View.VISIBLE);
-        ImageView imageView1 = (ImageView) findViewById(R.id.imageView);
         imageView1.setVisibility(View.GONE);
-        EditText t1 = (EditText) findViewById(R.id.editTextTextMultiLine);
+
         t1.setVisibility(View.GONE);
     }
 }
